a3076602b513133675defeb1a0a1065586c20966
BlackPenguin <dkstmdwnswns@naver.com>
2022-05-25 20:46:30 +0900

    Add Description

--- Assets/DeepRunningManager.cs
+++ Assets/DeepRunningManager.cs
@@ -12,11 +12,17 @@ public class DeepRunningManager : MonoBehaviour
     public StickManMoveSaveData saveData;
 
     string fileName = "SaveData";
+    private void Start()
+    {
+        //Time.timeScale = 10;
+    }
+    #region FixedUpdate
     private void FixedUpdate()
     {
         if (instance == null)
             instance = this;
 
+        //만약 씬에 모든 딥러닝 모델이 딥러닝이 끝났을경우 러닝을 다시 시작합니다.
         CharacterMove[] characters = FindObjectsOfType<CharacterMove>();
 
         allOk = true;
@@ -33,31 +39,38 @@ public class DeepRunningManager : MonoBehaviour
             GameReset();
         }
     }
+    #endregion
+    /// <summary>
+    /// 게임을 리셋 시켜줍니다.
+    /// </summary>
+    #region GameReset
     void GameReset()
     {
-
+        //저장된 값을 내립차순으로 정렬합니다 (높은것부터 낮은거 순)
         var data = from save in saveData.data
                    orderby save.resultValue descending
                    select save;
 
+        //상위 10개의 데이터만 남기고 나머지를 잘라서 리스트로 저장합니다.
         saveData.data = data.Take(10).ToList();
 
         string json = JsonUtility.ToJson(saveData);
         string path = Application.persistentDataPath + "/" + fileName + ".Json";
-
         File.WriteAllText(path, json);
 
+        //씬에 존재하는 모델에 갯수만큼 반복하여 모델을 다시 제작합니다.
         CharacterMove[] characters = FindObjectsOfType<CharacterMove>();
         foreach (CharacterMove character in characters)
         {
             if (character.originalCharacter)
             {
-                CharacterMove obj = Instantiate(characterPrefab, Vector3.zero, Quaternion.identity);
+                CharacterMove obj = Instantiate(characterPrefab, character.startPos, Quaternion.identity);
                 obj.originalCharacter = true;
             }
             else
-                Instantiate(characterPrefab, Vector3.zero, Quaternion.identity);
+                Instantiate(characterPrefab, character.startPos, Quaternion.identity);
             Destroy(character.gameObject);
         }
     }
+    #endregion
 }
